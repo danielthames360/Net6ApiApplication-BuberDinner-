218f99403ebd6d8c47b5358ee7096707ae9b61eb
Daniel <Daniel@DESKTOP-TS083U4>
2022-08-02 21:36:04 -0400

    Add project files.

---
+++ BuberDinner.Api/Program.cs
@@ -0,0 +1,20 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+{
+    builder.Services.AddApplication()
+            .AddInfrastructure();
+
+    builder.Services.AddControllers();
+}
+
+
+var app = builder.Build();
+{
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
